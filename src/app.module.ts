f0539825123f9311a4da91e1456d36f297806390
Kim Doomin <kimduumin@gmail.com>
2022-11-04 19:05:57 +0900

    delete all folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,6 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { TwitchModule } from './twitch/twitch.module';
-import { ConfigModule } from './config/config.module';
-import { BoardModule } from './board/board.module';
-import { UsersModule } from './users/users.module';
-
 @Module({
-  imports: [TwitchModule, ConfigModule, BoardModule, UsersModule],
+  imports: [],
   controllers: [],
 })
 export class AppModule {}
